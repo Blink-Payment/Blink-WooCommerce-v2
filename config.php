0e00fa9f6c1f353cbc79725ef043806b069ad4a7
Gourab Kirtania <gourab-mac@Gourabs-MacBook-Pro.local>
2023-03-03 15:18:34 +0530

    remove test host URL

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 return [
     'method_title' => 'Blink',
     'method_description' => 'Pay securely via Credit / Debit Card / Direct Debit / Open banking with Blink',
-    'host_url' => 'https://dev.blinkpayment.co.uk',
+    'host_url' => 'https://secure.blinkpayment.co.uk',
 ];
