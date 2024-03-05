29f0af933b1edc4dcb9ec748da822a0b43e90eee
gourab-cn <gourab@capitalnumbers.com>
2024-03-05 15:26:33 +0530

    Refund code

--- assets/js/admin-scripts.js
+++ assets/js/admin-scripts.js
@@ -1,181 +1,35 @@
 jQuery(function($) {
 
-    var blink_checkout_form = {
-        $form: jQuery('form.woocommerce-checkout'),
-        init: function() {
-            jQuery(document.body).on('updated_checkout', this.updated_checkout);
-        },
-        updated_checkout: function() {
-
-            var paymentMode = jQuery('input[name=payment_method]:checked').val();
-            if (paymentMode == 'blink') {
-                jQuery('#place_order').hide();
-            } else {
-                jQuery('#place_order').show();
-            }
-
-            var paymentBy = blink_checkout_form.$form.find('input[name=payment_by]').val();
-
-            if (paymentBy != '') {
-                blink_checkout_form.$form.submit();
-                return;
-            }
-        }
-    };
-
-    blink_checkout_form.init();
-
-    jQuery(document).ready(function() {
-        if ($('#blink-card').length) {
-            var $form = $('#blink-card');
-            var auto = {
-                autoSetup: true,
-                autoSubmit: true,
+    // JavaScript code to handle cancel transaction AJAX call
+    jQuery(document).ready(function($) {
+        // Function to handle cancel transaction AJAX call
+        $('.cancel-order').on('click', function(e) {
+            e.preventDefault();
+            $(this).after(`<div class="loader-container"><img src="${blinkOrders.spin_gif}" alt="Processing.."></div>`);
+            var orderId = $(this).data('order-id');
+            var data = {
+                action: 'cancel_transaction',
+                cancel_order: blinkOrders.cancel_order,
+                order_id: orderId
             };
-            try {
-                var hf = $form.hostedForm(auto);
-                var screen_width = (window && window.screen ? window.screen.width : '0');
-                var screen_height = (window && window.screen ? window.screen.height : '0');
-                var screen_depth = (window && window.screen ? window.screen.colorDepth : '0');
-                var language = (window && window.navigator ? (window.navigator.language ? window.navigator
-                    .language : window.navigator.browserLanguage) : '');
-                var java = (window && window.navigator ? navigator.javaEnabled() : false);
-                var timezone = (new Date()).getTimezoneOffset();
 
-                $form.find('input[name=customer_name]').val(order_params.customer_name);
-                $form.find('input[name=customer_email]').val(order_params.customer_email);
-                $form.find('input[name=customer_address]').val(order_params.billing_address_1 + ', ' + order_params.billing_address_2);
-                $form.find('input[name=customer_postcode]').val(order_params.billing_postcode);
-                $form.find('input[name=device_timezone]').val(timezone);
-                $form.find('input[name=device_capabilities]').val('javascript' + (java ? ',java' : ''));
-                $form.find('input[name=device_accept_language]').val(language);
-                $form.find('input[name=device_screen_resolution]').val(screen_width + 'x' + screen_height + 'x' +
-                    screen_depth);
-                $form.find('input[name=remote_address]').val(blink_params.remoteAddress);
+            // AJAX call to cancel transaction
+            $.post(blinkOrders.ajaxurl, data, function(response) {
 
-            } catch (e) {
-                //Add your exception handling code here
-            }
-        }
+                $('.loader-container').remove();
 
-        if ($('#blink-debit').length) {
-            try {
-                var $form = $('#blink-debit');
-
-                $form.find('input[name=given_name]').val(order_params.billing_first_name);
-                $form.find('input[name=family_name]').val(order_params.billing_last_name);
-                $form.find('input[name=email]').val(order_params.billing_email);
-                $form.find('input[name=account_holder_name]').val(order_params.customer_name);
-                $form.find('input[name=customer_address]').val(order_params.billing_address_1 + ', ' + order_params.billing_address_2);
-                $form.find('input[name=customer_postcode]').val(order_params.billing_postcode);
-            } catch (e) {
-                //Add your exception handling code here
-            }
-        }
-        if ($('#blink-open').length) {
-            try {
-                var $form = $('#blink-open');
-                $form.find('input[name=user_name]').val(order_params.customer_name);
-                $form.find('input[name=user_email]').val(order_params.customer_email);
-                $form.find('input[name=customer_address]').val(order_params.billing_address_1 + ', ' + order_params.billing_address_2);
-                $form.find('input[name=customer_postcode]').val(order_params.billing_postcode);
-            } catch (e) {
-                //Add your exception handling code here
-            }
-        }
-
-        var paymentMode = jQuery('input[name=payment_method]:checked').val();
-        if (paymentMode == 'blink') {
-            jQuery('#place_order').hide();
-        } else {
-            jQuery('#place_order').show();
-        }
-    });
-
-    if (jQuery(".blink-api-section").width() < 500)
-        jQuery('.blink-api-section').addClass('responsive-screen');
-    else
-        jQuery('.blink-api-section').removeClass('responsive-screen');
-
-    jQuery('form.checkout').on('change', 'input[name="payment_method"]', function() {
-        var paymentMode = jQuery('input[name=payment_method]:checked').val();
-        if (paymentMode == 'blink') {
-            jQuery('#place_order').hide();
-        } else {
-            jQuery('#place_order').show();
-        }
-    });
-});
-
-// Resize Function 
-jQuery(document).ready(blinkfunction);
-jQuery(window).on('resize', blinkfunction);
-
-function blinkfunction() {
-    // do whatever
-    if (jQuery(".blink-api-section").width() < 500)
-        jQuery('.blink-api-section').addClass('responsive-screen');
-    else
-        jQuery('.blink-api-section').removeClass('responsive-screen');
-}
-
-var blink_order_review_form = {
-    init: function() {
-        var paymentMode = jQuery('input[name=payment_method]:checked').val();
-        if (paymentMode == 'blink') {
-            jQuery('#place_order').hide();
-        } else {
-            jQuery('#place_order').show();
-        }
-
-        var paymentBy = jQuery('input[name=payment_by]').val();
-        console.log(paymentBy);
-
-        if (paymentBy != '') {
-            jQuery('#order_review').submit();
-            return;
-        }
-    }
-};
-
-
-var updatePaymentBy = function(data) {
-    var $form = jQuery('#payment_by').closest('form');
-    jQuery('#payment_by').val(data);
-
-    if ($form[0].id == 'order_review') {
-        blink_order_review_form.init();
-    } else {
-        jQuery(document.body).trigger('update_checkout');
-    }
-}
-
-// JavaScript code to handle cancel transaction AJAX call
-jQuery(document).ready(function($) {
-    // Function to handle cancel transaction AJAX call
-    $('.cancel-order').on('click', function(e) {
-        e.preventDefault();
-        $(this).after(`<div class="loader-container"><img src="${blinkOrders.spin_gif}" alt="Processing.."></div>`);
-        var orderId = $(this).data('order-id');
-        var data = {
-            action: 'cancel_transaction',
-            cancel_order: blinkOrders.cancel_order,
-            order_id: orderId
-        };
-
-        // AJAX call to cancel transaction
-        $.post(blinkOrders.ajaxurl, data, function(response) {
-
-            $('.loader-container').remove();
-
-            if (response.success) {
-                // Reload page or perform other actions
-                location.reload();
-            } else {
-                // Handle error
-                console.log(response);
-                alert(response.data.message ? 'Failed to cancel Transaction: ' + response.data.message : response.data);            }
+                if (response.success) {
+                    // Reload page or perform other actions
+                    location.reload();
+                } else {
+                    // Handle error
+                    console.log(response);
+                    alert(response.data.message ? 'Failed to cancel Transaction: ' + response.data.message : response.data);            }
+            });
         });
     });
+
 });
+
+
 
