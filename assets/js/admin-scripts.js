7390c22b47655a3d3e702c99e9e05856ff5838a3
Mohsin <119297653+mshah-blink@users.noreply.github.com>
2024-03-19 11:22:03 +0000

    Update admin-scripts.js

--- assets/js/admin-scripts.js
+++ assets/js/admin-scripts.js
@@ -1,35 +1,31 @@
-jQuery(function($) {
-
+jQuery(function ($) {
     // JavaScript code to handle cancel transaction AJAX call
-    jQuery(document).ready(function($) {
+    jQuery(document).ready(function ($) {
         // Function to handle cancel transaction AJAX call
-        $('.cancel-order').on('click', function(e) {
+        $(".cancel-order").on("click", function (e) {
             e.preventDefault();
-            $(this).after(`<div class="loader-container"><img src="${blinkOrders.spin_gif}" alt="Processing.."></div>`);
-            var orderId = $(this).data('order-id');
+            $(this).after(
+                `<div class="loader-container"><img src="${blinkOrders.spin_gif}" alt="Processing.."></div>`
+            );
+            var orderId = $(this).data("order-id");
             var data = {
-                action: 'cancel_transaction',
+                action: "cancel_transaction",
                 cancel_order: blinkOrders.cancel_order,
-                order_id: orderId
+                order_id: orderId,
             };
 
             // AJAX call to cancel transaction
-            $.post(blinkOrders.ajaxurl, data, function(response) {
-
-                $('.loader-container').remove();
-
+            $.post(blinkOrders.ajaxurl, data, function (response) {
+                $(".loader-container").remove();
                 if (response.success) {
                     // Reload page or perform other actions
                     location.reload();
                 } else {
                     // Handle error
                     console.log(response);
-                    alert(response.data.message ? 'Failed to cancel Transaction: ' + response.data.message : response.data);            }
+                    alert(response.data.message ? "Failed to cancel Transaction: " + response.data.message : response.data);
+                }
             });
         });
     });
-
 });
-
-
-
